68115ed27d1b6d4d947b0ff4f1847f929edffe6e
JordonGiusto <jordon.giusto@gmail.com>
2024-06-25 15:27:57 -0700

    about Rendering

--- aboutRenderer.js
+++ aboutRenderer.js
@@ -5,11 +5,20 @@ async function showJson(){
     let people = await json.json()
 
     let template = document.querySelector("#aboutContainer")
-
-
+    document.querySelector("#aboutParent").innerHTML = ""
     template.querySelector("#name").textContent = people[0].name
     template.querySelector("#description").textContent = people[0].description
     template.querySelector("#profilePic").src = people[0].photo
+
+    for (let index = 0; index < people.length; index++) {
+        const profile = people[index];
+        template.querySelector("#name").textContent = profile.name
+        template.querySelector("#description").textContent = profile.description
+        template.querySelector("#profilePic").src = profile.photo
+        document.querySelector("#aboutParent").appendChild(template)
+        template = template.cloneNode(true)
+
+    }
 
 }
 
