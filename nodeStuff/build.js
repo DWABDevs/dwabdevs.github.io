c675b94939c1d550bc069d9dbefa07cf345fde57
JordonGiusto <jordon.giusto@gmail.com>
2024-06-26 00:20:13 -0700

    fixed meta

--- nodeStuff/build.js
+++ nodeStuff/build.js
@@ -4,6 +4,7 @@ const html_parce = require('node-html-parser')
 const { parse } = html_parce
 
 var aliases = require("./aliases.json")
+const { error } = require('node:console')
 
 
 
@@ -25,12 +26,27 @@ for (let index = 0; index < aliases.pages.length; index++) {
         templateText = fs.readFileSync("../templates/" + templateName, 'utf8')
 
         try {
-            placeholder = root.querySelector(`#${templateName.split('.')[0]}`)
+            placeholder = root.querySelector('body').querySelector(`#${templateName.split('.')[0]}`)
             placeholder.replaceWith(templateText)
-            console.log(`place ${templateName} in ${pageName}`)
+            console.log(`place ${templateName} in ${pageName} body`)
         }
         catch {
 
+        }
+
+        
+        try {
+            placeholder = root.querySelector('head').querySelector(`#${templateName.split('.')[0]}`)
+            if(placeholder == undefined)
+                throw new Error("not supp")
+            
+            root.querySelector('head').removeChild(placeholder)
+
+            root.querySelector("head").innerHTML += templateText
+            
+            console.log(`place ${templateName} in ${pageName} head`)
+        }
+        catch (e) {
         }
 
     }
